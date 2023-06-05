9e41bdcce5254f6cee9a4e817f8937bb381683bb
Amaan <112427362+amaan1dev@users.noreply.github.com>
2023-06-05 16:51:58 +0530

    ratings updated in DB

--- src/backend/db/products.js
+++ src/backend/db/products.js
@@ -14,6 +14,7 @@ export const products = [
     seller: "Nike",
     price: "₹ 10,795.00",
     categoryName: "Performance",
+    rating: "4"
   },
   {
     image:
@@ -23,6 +24,7 @@ export const products = [
     seller: "Vans",
     price: "₹ 5,999.00",
     categoryName: "Low-Top",
+    rating: "3"
   },
   {
     image:
@@ -32,6 +34,7 @@ export const products = [
     seller: "Converse",
     price: "₹ 4,999.00",
     categoryName: "High-Top",
+    rating: "3"
   },
   {
     image:
@@ -41,6 +44,7 @@ export const products = [
     seller: "Adidas",
     price: "₹ 14,999.00",
     categoryName: "Performance",
+    rating: "2"
   },
   {
     image:
@@ -50,6 +54,7 @@ export const products = [
     seller: "Nike",
     price: "₹ 6,999.00",
     categoryName: "Skateboards",
+    rating: "5"
   },
 
   {
@@ -59,6 +64,7 @@ export const products = [
     seller: "Nike",
     price: "₹ 13,499.00",
     categoryName: "High-Top",
+    rating: "2"
   },
   {
     image: "https://cdn.shopify.com/s/files/1/0659/0722/8923/products/1-169.jpg?v=1675969206&width=493",
@@ -67,6 +73,7 @@ export const products = [
     seller: "Vans",
     price: "₹ 5,499.00",
     categoryName: "High-Top",
+    rating: "4"
   },
   {
     image: "https://cdn.shopify.com/s/files/1/0659/0722/8923/products/1_7c72d0e3-144a-4302-bc2d-2e2f16756f6c.jpg?v=1679642125&width=493",
@@ -75,6 +82,7 @@ export const products = [
     seller: "Puma",
     price: "₹ 3,999.00",
     categoryName: "Low-Top",
+    rating: "4"
   },
   {
     image: "https://cdn.shopify.com/s/files/1/0659/0722/8923/products/1_fa359ade-7ef6-4760-980c-ddeb8175eb02.jpg?v=1680269920&width=493",
@@ -83,6 +91,8 @@ export const products = [
     seller: "Reebok",
     price: "₹ 4,599.00",
     categoryName: "Low-Top",
+    rating: "3"
+
   },
   {
     image: "https://cdn.shopify.com/s/files/1/0659/0722/8923/files/1_e8f22310-522f-4081-8c18-c36982fc48eb.jpg?v=1685538808&width=493",
@@ -91,6 +101,7 @@ export const products = [
     seller: "Adidas",
     price: "₹ 6,499.00",
     categoryName: "Low-Top",
+    rating: "2"
   },
   // Add more products below
   {
@@ -100,6 +111,7 @@ export const products = [
     seller: "Nike",
     price: "₹ 8,999.00",
     categoryName: "High-Top",
+    rating: "5"
   },
   {
     image: "https://cdn.shopify.com/s/files/1/0659/0722/8923/products/1_403fe654-20b1-471a-afae-d8441db2298f.jpg?v=1679311945&width=493",
@@ -108,6 +120,7 @@ export const products = [
     seller: "Converse",
     price: "₹ 4,299.00",
     categoryName: "Low-Top",
+    rating: "2"
   },
   {
     image: "https://cdn.shopify.com/s/files/1/0659/0722/8923/products/1-19-1.jpg?v=1675975710&width=493",
@@ -116,6 +129,7 @@ export const products = [
     seller: "Puma",
     price: "₹ 7,199.00",
     categoryName: "Performance",
+    rating: "1"
   },
   {
     image: "https://cdn.shopify.com/s/files/1/0659/0722/8923/products/1-68_50ee3a5b-8559-493e-a56f-c5248554ba59.jpg?v=1675978061&width=493",
@@ -124,6 +138,7 @@ export const products = [
     seller: "Adidas",
     price: "₹ 9,999.00",
     categoryName: "Performance",
+    rating: "3"
   },
   {
     image: "https://cdn.shopify.com/s/files/1/0659/0722/8923/files/1_c55419eb-d729-40e3-8ef2-a773a4c7af2d.jpg?v=1683120203&width=493",
@@ -132,6 +147,7 @@ export const products = [
     seller: "Nike",
     price: "₹ 7,999.00",
     categoryName: "Skateboards",
+    rating: "5"
   },
   {
     image: "https://cdn.shopify.com/s/files/1/0659/0722/8923/products/1-152_00dabb71-03ed-421c-bc14-7ddf7c1ab5a6.jpg?v=1677044396&width=493",
@@ -140,6 +156,7 @@ export const products = [
     seller: "Vans",
     price: "₹ 5,999.00",
     categoryName: "Skateboards",
+    rating: "3"
   },
   {
     image: "https://cdn.shopify.com/s/files/1/0659/0722/8923/files/1_8b2a286f-964a-440c-8a34-71b4107fde20.jpg?v=1683742047&width=493",
@@ -148,6 +165,7 @@ export const products = [
     seller: "Vans",
     price: "₹ 6,499.00",
     categoryName: "Skateboards",
+    rating: "4"
   },
   {
     image: "https://cdn.shopify.com/s/files/1/0659/0722/8923/files/1_a39eea6a-e9ae-4831-a1f4-37483707f243.jpg?v=1684224850&width=493",
@@ -156,6 +174,7 @@ export const products = [
     seller: "Converse",
     price: "₹ 5,299.00",
     categoryName: "Skateboards",
+    rating: "2"
   },
 
   {
@@ -165,6 +184,7 @@ export const products = [
     seller: "Converse",
     price: "₹ 4,799.00",
     categoryName: "Converse",
+    rating: "5"
   },
   {
     image: "https://cdn.shopify.com/s/files/1/0659/0722/8923/products/36915523-3.jpg?v=1675956835&width=493",
@@ -173,5 +193,6 @@ export const products = [
     seller: "Puma",
     price: "₹ 4,199.00",
     categoryName: "Converse",
+    rating: "2"
   }
 ];
